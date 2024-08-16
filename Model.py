d40a4bcfe53e1fbbc2a3a637d770b77bb71950ed
Avik Trilok <109292740+avik-trilok-12@users.noreply.github.com>
2024-08-17 00:23:26 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -6,41 +6,41 @@ from tensorflow.keras import layers,models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 
-"""## Dataset Loading"""
-
-#defining paths to dataset for training, testing and validation
+#Defining paths to dataset for training, testing and validation
 dataset_dir = 'E:/new dataset'
 test_data_dir = 'E:/training_set'
 
-#defining image width and image height along with batch size
+#Defining image width and image height along with batch size
 img_width, img_height = 128, 128
 batch_size = 32
 
-#stating validation split = 0.3 for train-test split
+#Stating validation split = 0.3 for train-test split
 train_datagen = ImageDataGenerator(rescale=1./255,validation_split=0.3,)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-#creating training dataset
+#Creating training dataset
 train_generator = train_datagen.flow_from_directory(
     dataset_dir,
     target_size=(img_width,img_height),
     batch_size=batch_size,
     class_mode='categorical',
     subset='training')
-#creating validation dataset
+#Creating validation dataset
 validation_generator = train_datagen.flow_from_directory(
     dataset_dir,
     target_size=(img_width,img_height),
     batch_size=batch_size,
     class_mode='categorical',
     subset='validation')
-#creating test dataset
+#Creating test dataset
 test_generator = test_datagen.flow_from_directory(
     test_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='categorical')
 
+
+#Model Architecture
 model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)))  # Updated input_shape
@@ -61,6 +61,7 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 model.summary()
 
+#Training the model
 history = model.fit(train_generator, validation_data=validation_generator, epochs=100, steps_per_epoch=15)
 
 test_loss, test_accuracy = model.evaluate(test_generator)
